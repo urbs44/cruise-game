5cc2ba2f02398ef2ee6de1f66d425f95daf27254
Matthew Urbano <matt.urbano@uniworld.com>
2025-02-26 10:50:26 +0100

    update

--- Uniworld_Cruise_Game.js
+++ Uniworld_Cruise_Game.js
@@ -70,6 +70,10 @@ const config = {
             debug: false
         }
     },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     scene: [BootScene, PreloadScene, MainScene]
 };
 
